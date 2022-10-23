70c1ffee691f944290732905ff3b8a121a517a79
Carl Seelye <cseelye@gmail.com>
2022-10-22 21:36:05 -0400

    Fix typo

--- install-docker.sh
+++ install-docker.sh
@@ -5,4 +5,4 @@ shopt -s inherit_errexit
 latest=$(curl -fsSL https://download.docker.com/linux/static/stable/$(uname -m)/ | grep '<a' | grep -v rootless | grep -Po 'href="docker.+"' | sort -V | tail -n1 | cut -d'"' -f2)
 
 curl -fsSL https://download.docker.com/linux/static/stable/$(uname -m)/${latest} | \
-     | tar -xz --strip-components=1 -C /usr/local/bin/ docker/docker
+     tar -xz --strip-components=1 -C /usr/local/bin/ docker/docker
