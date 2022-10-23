07025407033021ff80e7bceaa5678c3d6423216f
Carl Seelye <cseelye@gmail.com>
2022-10-23 13:12:14 -0400

    debug

--- install-docker.sh
+++ install-docker.sh
@@ -2,6 +2,8 @@
 set -euETo pipefail
 shopt -s inherit_errexit
 
+uname -m
+
 latest=$(curl -fsSL https://download.docker.com/linux/static/stable/$(uname -m)/ | grep '<a' | grep -v rootless | grep -Po 'href="docker.+"' | sort -V | tail -n1 | cut -d'"' -f2)
 
 curl -fsSL https://download.docker.com/linux/static/stable/$(uname -m)/${latest} | \
