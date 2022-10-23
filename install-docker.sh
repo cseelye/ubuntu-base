d7e52bf28298764452afa55dbc501087c71f1b86
Carl Seelye <cseelye@gmail.com>
2022-10-23 13:35:33 -0400

    Fix docker client install on 32-bit ARM

--- install-docker.sh
+++ install-docker.sh
@@ -3,8 +3,12 @@ set -euETo pipefail
 shopt -s inherit_errexit
 
 uname -m
+mach=$(uname -m)
+if [[ ${mach} == "armv7l" ]]; then
+    mach="armhf"
+fi
 
-latest=$(curl -fsSL https://download.docker.com/linux/static/stable/$(uname -m)/ | grep '<a' | grep -v rootless | grep -Po 'href="docker.+"' | sort -V | tail -n1 | cut -d'"' -f2)
+latest=$(curl -fsSL https://download.docker.com/linux/static/stable/${mach}/ | grep '<a' | grep -v rootless | grep -Po 'href="docker.+"' | sort -V | tail -n1 | cut -d'"' -f2)
 
-curl -fsSL https://download.docker.com/linux/static/stable/$(uname -m)/${latest} | \
+curl -fsSL https://download.docker.com/linux/static/stable/${mach}/${latest} | \
      tar -xz --strip-components=1 -C /usr/local/bin/ docker/docker
