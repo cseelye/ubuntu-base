96edeeb038972bd0b1d274981b2fd2c2f9977dd6
Carl Seelye <cseelye@gmail.com>
2022-11-30 10:16:04 -0500

    Show all output when building images locally; refactor and simplify vsc extension install

--- Makefile
+++ Makefile
@@ -5,42 +5,42 @@ test-build: test-build20 test-build22 ;
 
 .PHONY: test-build22
 test-build22:
-	docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg UBUNTU_VERSION=22.04 --target=base . && \
-	docker buildx build --platform linux/amd64,linux/arm64 --build-arg UBUNTU_VERSION=22.04 --target=base-dev . && \
-	docker buildx build --platform linux/amd64,linux/arm64 --build-arg UBUNTU_VERSION=22.04 --target=base-ansible-dev .
+	docker buildx build --progress=plain --platform linux/amd64,linux/arm64,linux/arm/v7 --build-arg UBUNTU_VERSION=22.04 --target=base . && \
+	docker buildx build --progress=plain --platform linux/amd64,linux/arm64 --build-arg UBUNTU_VERSION=22.04 --target=base-dev . && \
+	docker buildx build --progress=plain --platform linux/amd64,linux/arm64 --build-arg UBUNTU_VERSION=22.04 --target=base-ansible-dev .
 .PHONY: test-build20
 test-build20:
-	docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --target=base . && \
-	docker buildx build --platform linux/amd64,linux/arm64 --target=base-dev . && \
-	docker buildx build --platform linux/amd64,linux/arm64 --target=base-ansible-dev .
+	docker buildx build --progress=plain --platform linux/amd64,linux/arm64,linux/arm/v7 --target=base . && \
+	docker buildx build --progress=plain --platform linux/amd64,linux/arm64 --target=base-dev . && \
+	docker buildx build --progress=plain --platform linux/amd64,linux/arm64 --target=base-ansible-dev .
 
 #
 # Build individual images
 #
 .PHONY:base-intel
 base-intel:
-	docker buildx build --load --platform linux/amd64 --target base -t local/ubuntu-base-intel .
+	docker buildx build --load --progress=plain --platform linux/amd64 --target base -t local/ubuntu-base-intel .
 
 .PHONY:base-arm
 base-arm:
-	docker buildx build --load --platform linux/arm/v7 --target base -t local/ubuntu-base-arm .
+	docker buildx build --load --progress=plain --platform linux/arm/v7 --target base -t local/ubuntu-base-arm .
 
 .PHONY:base-arm64
 base-arm64:
-	docker buildx build --load --platform linux/arm64 --target base -t local/ubuntu-base-arm64 .
+	docker buildx build --load --progress=plain --platform linux/arm64 --target base -t local/ubuntu-base-arm64 .
 
 .PHONY:dev-intel
 dev-intel:
-	docker buildx build --load --platform linux/amd64 --target base-dev -t local/ubuntu-base-dev-intel .
+	docker buildx build --load --progress=plain --platform linux/amd64 --target base-dev -t local/ubuntu-base-dev-intel .
 
 .PHONY:dev-arm64
 dev-arm64:
-	docker buildx build --load --platform linux/arm64 --target base-dev -t local/ubuntu-base-dev-arm64 .
+	docker buildx build --load --progress=plain --platform linux/arm64 --target base-dev -t local/ubuntu-base-dev-arm64 .
 
 .PHONY:ansible-dev-intel
 ansible-dev-intel:
-	docker buildx build --load --platform linux/amd64 --target base-ansible-dev -t local/ubuntu-base-dev-intel .
+	docker buildx build --load --progress=plain --platform linux/amd64 --target base-ansible-dev -t local/ubuntu-base-dev-intel .
 
 .PHONY:ansible-dev-arm64
 ansible-dev-arm64:
-	docker buildx build --load --platform linux/arm64 --target base-ansible-dev -t local/ubuntu-base-dev-arm64 .
+	docker buildx build --load --progress=plain --platform linux/arm64 --target base-ansible-dev -t local/ubuntu-base-dev-arm64 .
--- vscode.sh
+++ vscode.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euETo pipefail
 shopt -s inherit_errexit
+shopt -s lastpipe
 
 mach=$(uname -m)
 if [[ ${mach} == "x86_64" ]]; then
@@ -14,7 +15,12 @@ else
     exit 0
 fi
 
-# For 22.04, we need to get libssl1.1 and manually install
+
+#
+# Live Share prerequisites
+#
+echo "* * *  Installing Live Share prerequisites"
+# For 22.04, we need to get libssl1.1 and manually install before the vsls script
 if [[ ${UBUNTU_VERSION} == "22.04" ]]; then
     if [[ ${mach} == "x86_64" ]]; then
         curl -sSfLo /tmp/libssl.deb http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.16_${arch1}.deb
@@ -25,19 +31,23 @@ if [[ ${UBUNTU_VERSION} == "22.04" ]]; then
     rm -f /tmp/libssl.deb
 fi
 
-# Install live share prerequisites
+# Install live share prerequisites from official script
 curl -sSfLo /tmp/vsls-reqs https://aka.ms/vsls-linux-prereq-script
 chmod +x /tmp/vsls-reqs
-/tmp/vsls-reqs | sed -u 's/^/vsls | /'
+/tmp/vsls-reqs
 rm -f /vsls-reqs
 
+
+#
+# VS Code Server
+#
+echo "* * *  Installing VS Code Server"
 # Get the most recent version tags from the vscode repo
-set -x
 repo="microsoft/vscode"
 version_count=3 # Get the most recent 3 versions
 all_tags=$(git ls-remote --tags https://github.com/${repo}.git | grep 'refs/tags/[0-9]')
 recent_tags=($(echo "${all_tags}" | sed 's|.*/||' | grep -v '\^' | sort -rV | head -n${version_count}))
-ext_installed=0
+newest_ver=""
 for tag_ver in "${recent_tags[@]}"; do
     # Account for both lightweight and annotated tags
     lines=$(echo "${all_tags}" | grep "${tag_ver}")
@@ -46,6 +56,9 @@ for tag_ver in "${recent_tags[@]}"; do
     else
         tag_sha=$(echo "${lines}" | grep "${tag_ver}\^{}" | awk '{print $1}')
     fi
+    if [[ -z ${newest_ver} ]]; then
+        newest_ver=${tag_sha}
+    fi
     echo "Installing code-server ${tag_ver} - ${tag_sha}"
     curl -fsSLo /tmp/vscs.tgz "https://update.code.visualstudio.com/commit:${tag_sha}/server-linux-${arch2}/stable"
     mkdir -p ~/.vscode-server/bin/"${tag_sha}"
@@ -53,24 +66,26 @@ for tag_ver in "${recent_tags[@]}"; do
     tar xzf /tmp/vscs.tgz --no-same-owner --strip-components=1
     popd >/dev/null
     rm -f /tmp/vscs.tgz
-    # Install extensions
-    if [[ ${ext_installed} -eq 0 ]]; then
-        (
-        export PATH=${PATH}:~/.vscode-server/bin/${tag_sha}/bin
-        set +e
-        finished=0
-        until [[ ${finished} == 1 ]]; do
-            sleep 5
-            #code-server --install-extension ms-vsliveshare.vsliveshare
-            code-server --install-extension cseelye.vscode-allofthem
-            code-server --install-extension ms-python.python
-            code-server --install-extension ms-python.vscode-pylance
-            code-server --install-extension iliazeus.vscode-ansi
-            code-server --install-extension eriklynd.json-tools
-            code-server --install-extension hangxingliu.vscode-systemd-support
-            finished=1
-        done
-        ) | sed -u 's/^/    /'
-        ext_installed=1
-    fi
+done
+echo "newest_ver=${newest_ver}"
+
+
+#
+# VS Code extensions
+#
+echo "* * *  Installing VS Code Extensions"
+extensions=(
+    cseelye.vscode-allofthem
+    ms-python.python
+    ms-python.vscode-pylance
+    iliazeus.vscode-ansi
+    eriklynd.json-tools
+    hangxingliu.vscode-systemd-support
+    redhat.vscode-yaml
+    ms-vsliveshare.vsliveshare
+)
+
+export PATH=${PATH}:~/.vscode-server/bin/${newest_ver}/bin
+for extname in "${extensions[@]}"; do
+    code-server --install-extension ${extname} || success=0
 done
